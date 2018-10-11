2c152bc51d100438246c9dffe83d67d0d98aafec
Anthony Clifton <aclifton@pillartechnology.com>
2018-10-11 13:44:02 -0500

    Added soap with wsdl and configuration that works

--- server.js
+++ server.js
@@ -1,15 +1,54 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
+const soap = require('soap');
+const bodyParser = require('body-parser');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-const app = express();
+var myService = {
+    MyService: {
+        MyPort: {
+            MyFunction: function(args) {
+                return {
+                    name: args.name,
+                    testParam: args.testParam
+                }
+            },
+            MyAsyncFunction: function(args, callback) {
+                callback({
+                    name: args.name
+                })
+            },
+            HeadersAwareFunction: function(args, cb, headers) {
+                return {
+                    name: headers.Token
+                }
+            },
+            reallyDetailedFunction: function(args, cb, headers, req) {
+                console.log('SOAP `reallyDetailedFunction` request from ' + req.connection.remoteAddress);
+                return {
+                    name: headers.Token
+                }
+            }
+        }
+    }
+}
+var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
 
-app.get('/', (req, res) => {
-    res.send('Cute little kitties YAY!');
+var server = http.createServer(function(request, response) {
+    response.end('404: Not found ' + request.url);
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}: ${PORT}`);
+server.listen(PORT);
+soap.listen(server, '/wsdl', myService, xml);
+
+var app = express();
+
+app.use(bodyParser.raw({type: function(){return true;}, limit: '5mb'}));
+
+app.listen(PORT+1, function() {
+    soap.listen(app, '/wsdl', myService, xml);
+});
