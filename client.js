8b96503f51197e1a059f8cb0dd6439e87d59cc27
Anthony Clifton <aclifton@pillartechnology.com>
2018-10-12 10:36:19 -0500

    working soap client and server

--- client.js
+++ client.js
@@ -5,8 +5,9 @@ var args = {name: 'value', testParam: 'something'};
 
 soap.createClient(url, function(err, client) {
     console.log('inside create');
+    console.log(client);
     client.MyFunction(args, function(err, result) {
         console.log('inside my function');
-        console.log(result);
+        console.log(JSON.stringify(result));
     }, {timeout: 5000});
 });
